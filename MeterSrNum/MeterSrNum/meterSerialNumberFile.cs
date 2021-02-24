2abe82e8bfd4303e2e6543ac737cdc99dddc4ae1
ravindra-rj <ravindrajaduu089@gmail.com>
2021-02-24 12:28:28 +0530

    method created for valid serial number of method

---
+++ MeterSrNum/MeterSrNum/meterSerialNumberFile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MeterSrNum
+{
+    public class SrNum
+    {
+        private string serial_num;
+        private int length;
+
+        private SrNum() { }
+
+        public SrNum(string meternum)
+        {
+            serial_num = meternum;
+            length = serial_num.Length;
+        }
+
+        public int get_len()
+        {
+           { return length; }
+        }
+
+        public bool srNumCheck()
+        {
+            bool lenCheck=true, numCheck=true,resultCheck=true;
+
+            foreach(char i in serial_num)
+            {
+                if(char.IsUpper(i) || char.IsNumber(i))
+                { numCheck = true; }
+                else
+                {
+                    numCheck = false;
+                    break;
+                }
+
+            }
+            if (length >= 8 && length <= 16)
+
+                lenCheck = true;
+            else
+                lenCheck = false;
+
+            resultCheck = lenCheck & numCheck;
+            return resultCheck;
+        }
+
+        static void Main(string[] args)
+        {
+
+           
+
+        }
+    }
+}
---
+++ MeterSrNum/MeterSrNumTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MeterSrNum;
+
+namespace MeterSrNumTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FirstNumber()
+        {
+            // place all AAA
+            //Arrange
+            string meternum = "SDERS00M680MLSSBY1254LJU012";
+
+            MeterSrNum.SrNum meter1 = new MeterSrNum.SrNum(meternum);
+
+            //Assert
+            bool finalResult = meter1.srNumCheck();
+            Assert.IsTrue(finalResult);
+        }
+    }
+}
