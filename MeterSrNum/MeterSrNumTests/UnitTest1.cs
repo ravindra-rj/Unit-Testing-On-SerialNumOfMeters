6fe977d443038a5cb67bff6958b1934091bfe106
ravindra-rj <ravindrajaduu089@gmail.com>
2021-02-24 12:34:34 +0530

    second test case added

--- MeterSrNum/MeterSrNumTests/UnitTest1.cs
+++ MeterSrNum/MeterSrNumTests/UnitTest1.cs
@@ -19,5 +19,19 @@ namespace MeterSrNumTests
             bool finalResult = meter1.srNumCheck();
             Assert.IsTrue(finalResult);
         }
+
+        [TestMethod]
+        public void SecondNumber()
+        {
+            // place all AAA
+            //Arrange
+            string meternum = "SSBY1254LJU012";
+
+            MeterSrNum.SrNum meter1 = new MeterSrNum.SrNum(meternum);
+
+            //Assert
+            bool finalResult = meter1.srNumCheck();
+            Assert.IsTrue(finalResult);
+        }
     }
 }
